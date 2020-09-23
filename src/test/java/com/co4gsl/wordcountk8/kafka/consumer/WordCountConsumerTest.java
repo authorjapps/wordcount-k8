7fde10e4954628ab79619c9a6a89df4e67476a2c
RPAYAL <rpayal@hotmail.com>
2020-09-23 01:03:09 +0100

    Added test for startbysubscribing to consumer.

--- src/test/java/com/co4gsl/wordcountk8/kafka/consumer/WordCountConsumerTest.java
+++ src/test/java/com/co4gsl/wordcountk8/kafka/consumer/WordCountConsumerTest.java
@@ -9,10 +9,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class WordCountConsumerTest {
@@ -49,6 +49,28 @@ class WordCountConsumerTest {
 
         // THEN
  //       assertEquals(1, updates.size());
+        assertTrue(consumer.closed());
+    }
+
+    @Test
+    void whenStartingBySubscribingToTopic_thenExpectUpdatesAreConsumedCorrectly() {
+        // GIVEN
+        consumer.schedulePollTask(() -> {
+            consumer.rebalance(Collections.singletonList(new TopicPartition(TOPIC, 0)));
+            consumer.addRecord(record(TOPIC, PARTITION, "check that out", "check this out"));
+        });
+        consumer.schedulePollTask(() -> wordCountConsumer.stop());
+
+        HashMap<TopicPartition, Long> startOffsets = new HashMap<>();
+        TopicPartition tp = new TopicPartition(TOPIC, PARTITION);
+        startOffsets.put(tp, 0L);
+        consumer.updateBeginningOffsets(startOffsets);
+
+        // WHEN
+        wordCountConsumer.startBySubscribing(TOPIC);
+
+        // THEN
+//       assertEquals(1, updates.size());
         assertTrue(consumer.closed());
     }
 
