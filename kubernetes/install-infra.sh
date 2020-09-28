a2fc2adebf1b2bbac814d86e6404454a453af43c
RPAYAL <rpayal@hotmail.com>
2020-09-28 22:32:10 +0100

    Changes made to use Confluient image for kafka and zk.

--- kubernetes/install-infra.sh
+++ kubernetes/install-infra.sh
@@ -5,10 +5,10 @@ kubectl create ns kafka-ns
 kubectl create ns wordapp-ns
 
 # Install Kafka + Zookeeper
-# kubectl create -f zookeeper.yml -n kafka-ns
-# kubectl create -f kafka.yml  -n kafka-ns
-kubectl create -f zookeeper_o.yml -n kafka-ns
-kubectl create -f kafka_o.yml  -n kafka-ns
+kubectl create -f zookeeper.yml -n kafka-ns
+kubectl create -f kafka.yml  -n kafka-ns
+# kubectl create -f zookeeper_o.yml -n kafka-ns
+# kubectl create -f kafka_o.yml  -n kafka-ns
 
 # Deploy application pod and service
 kubectl create -f app-service.yml -n wordapp-ns
